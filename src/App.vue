4f010284b50c530af6a50c147b4fc3f9bbe306d2
Chi Lin <chilin89117@gmail.com>
2017-10-12 12:51:18 -0700

    Minor clean-ups

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template lang="html">
+<template>
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -62,6 +62,3 @@
     },
   };
 </script>
-
-<style lang="css">
-</style>
